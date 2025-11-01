a4f4a89bd2bb8702e6b0f40fa69dbc0a52a2a215
joshuashao8 <shaojoshua8@gmail.com>
2025-11-01 16:16:17 -0400

    e

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import TeachableMachinePose from "./TeachableMachinePose.tsx";
+/*
+function App() {
+  return (
+    <div>
+      <TeachableMachinePose />
+    </div>
+  );
+}
 
-
-
-
-
-
-
-
-
-
-
+export default App;
+*/
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,7 +21,7 @@ function App() {
   return (
     <>
     
-      /*<h1>helloeeeeee</h1>
+      <h1>helloeeeeee</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -34,20 +35,18 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-*/
+
+
+      <button disabled={true}>Click me</button>
 
 
 
-      
     </>
   )
 }
 
-export default App
+export default App;
