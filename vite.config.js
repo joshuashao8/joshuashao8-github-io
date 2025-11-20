feb303f1d9f21f201acb000384c11141588af33c
Joshua Shao <shaojoshua8@gmail.com>
2025-11-19 21:30:14 -0500

    Changed build output to 'docs' for GitHub Pages compatibility

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/joshuashao8-github-io/",
   plugins: [react()],
+
+
+  build: {
+    outDir: 'docs' // Tell Vite to output files to a 'docs' folder instead of 'dist'
+  }
 })
